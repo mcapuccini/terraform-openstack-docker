ba45067abebe2b8a86d248930ed0356b4f799471
mcapuccini <marco.capuccini@riken.jp>
2020-05-28 11:28:50 +0200

    add docker host output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "floating_ip" {
   value = openstack_networking_floatingip_v2.floating_ip.address
 }
+
+output "docker_host" {
+  value = "ssh://${var.remote_user}:${openstack_networking_floatingip_v2.floating_ip.address}"
+}
